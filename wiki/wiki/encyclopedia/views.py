71ff6b229d26958102d5707705ae6be6002de237
empowering <empowering>
2020-08-12 00:02:28 +0900

    wiki project

---
+++ wiki/wiki/encyclopedia/views.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request):
+    return HttpResponse("Hello!")
